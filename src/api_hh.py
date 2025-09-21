5ef9dd36693c48f357e20612962a27769b510f39
Denis_Badin <denisbadin@yandex.ru>
2025-09-21 16:08:46 +0500

    Окночательный вариант (21.09.2025)

--- src/api_hh.py
+++ src/api_hh.py
@@ -1,20 +1,17 @@
-from typing import Any
-import json
 import requests
-from mypy.dmypy.client import request
 
 
-def vacansies_work (list_companies: list[dict[str, str]]) ->  list[dict]:
+def vacansies_work(list_companies: list[dict[str, str]]) -> list[dict]:
     """Функция получает список словарей и выводит по id вакансии с сайта hh.ru
     и далее формирует список словарей"""
     dict_vac_comp = []
     count_e = 1000
     for i_list in list_companies:
-        id_c = i_list['employer_id']
-        params = {'employer_id' : id_c}
-        list_vac = requests.get ('https://api.hh.ru/vacancies', params=params)
+        id_c = i_list["employer_id"]
+        params = {"employer_id": id_c}
+        list_vac = requests.get("https://api.hh.ru/vacancies", params=params)
         data = list_vac.json()
-        hh_vacancies  = data.get("items", [])
+        hh_vacancies = data.get("items", [])
         list_vacancy = []
         count_v = count_e
         for i in hh_vacancies:
@@ -31,20 +28,15 @@ def vacansies_work (list_companies: list[dict[str, str]]) ->  list[dict]:
             url = i.get("area").get("url")
 
             dict_vacancy = {
-                            'vacancy_id' : count_v,
-                            'name_vacancy' : nam,
-                            'salary' : salary,
-                            'description' : description,
-                            'url' : url}
-            count_v +=1
+                "vacancy_id": count_v,
+                "name_vacancy": nam,
+                "salary": salary,
+                "description": description,
+                "url": url,
+            }
+            count_v += 1
             list_vacancy.append(dict_vacancy)
-        dict_list_vac = {'employer_id' : id_c, 'list_vacancy' : list_vacancy}
+        dict_list_vac = {"employer_id": id_c, "list_vacancy": list_vacancy}
         count_e += 100
         dict_vac_comp.append(dict_list_vac)
     return dict_vac_comp
-
-
-
-
-
-
